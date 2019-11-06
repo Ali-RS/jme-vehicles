b83828613229ad7a964d78072195ee04b559b628
jayfella <jayfella@gmail.com>
2019-11-06 08:44:57 +0000

    Add bullet and stats debug toggle

--- src/main/java/com/jayfella/jme/vehicle/debug/DebugTabState.java
+++ src/main/java/com/jayfella/jme/vehicle/debug/DebugTabState.java
@@ -2,8 +2,9 @@ package com.jayfella.jme.vehicle.debug;
 
 import com.jayfella.jme.vehicle.MagicFormulaState;
 import com.jme3.app.Application;
+import com.jme3.app.StatsAppState;
 import com.jme3.app.state.BaseAppState;
-import com.simsilica.lemur.Button;
+import com.jme3.bullet.BulletAppState;
 import com.simsilica.lemur.Container;
 import com.simsilica.lemur.TabbedPanel;
 import com.simsilica.lemur.props.PropertyPanel;
@@ -17,23 +18,28 @@ public class DebugTabState extends BaseAppState {
     @Override
     protected void initialize(Application app) {
 
+        Container container = new Container();
+
+        BulletAppState bulletAppState = getState(BulletAppState.class);
+
         VehicleEditorState editorState = getState(VehicleEditorState.class);
         TabbedPanel tabbedPanel = editorState.getTabbedPanel();
-
-        Container container = new Container();
 
         EnginePowerGraphState enginePowerGraphState = getState(EnginePowerGraphState.class);
         TyreDataState tyreDataState = getState(TyreDataState.class);
 
         MagicFormulaState magicFormulaState = getState(MagicFormulaState.class);
 
+
+
         PropertyPanel propertyPanel = container.addChild(new PropertyPanel("glass"));
+        propertyPanel.addBooleanProperty("Display FPS", this, "displayFps");
+        propertyPanel.addBooleanProperty("Display Stats", this, "displayStats");
+        propertyPanel.addBooleanProperty("Bullet Debug", bulletAppState, "debugEnabled");
         propertyPanel.addBooleanProperty("Engine Graph", enginePowerGraphState, "enabled");
         propertyPanel.addBooleanProperty("Tyre Graph", tyreDataState, "enabled");
         propertyPanel.addBooleanProperty("Tyre Data", magicFormulaState, "vehicleDataEnabled");
         propertyPanel.addBooleanProperty("Center of Gravity", magicFormulaState, "centerOfGravityEnabled");
-
-
 
         tabbedPanel.addTab("Debug", container);
 
@@ -42,6 +48,27 @@ public class DebugTabState extends BaseAppState {
                 app.getCamera().getHeight() - 20,
                 0
         );
+    }
+
+    private boolean displayFps = false;
+    private boolean displayStats = false;
+
+    public boolean isDisplayFps() {
+        return displayFps;
+    }
+
+    public void setDisplayFps(boolean displayFps) {
+        this.displayFps = displayFps;
+        getState(StatsAppState.class).setDisplayFps(displayFps);
+    }
+
+    public boolean isDisplayStats() {
+        return displayStats;
+    }
+
+    public void setDisplayStats(boolean displayStats) {
+        this.displayStats = displayStats;
+        getState(StatsAppState.class).setDisplayStatView(displayStats);
     }
 
     @Override
