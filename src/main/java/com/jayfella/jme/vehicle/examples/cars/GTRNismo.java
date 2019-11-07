cdb3d01a661a9597a858bc2aafe8d0eeae5348db
jayfella <jayfella@gmail.com>
2019-11-07 16:03:47 +0000

    Tidy up GTRNismo a little.

--- src/main/java/com/jayfella/jme/vehicle/examples/cars/GTRNismo.java
+++ src/main/java/com/jayfella/jme/vehicle/examples/cars/GTRNismo.java
@@ -26,23 +26,19 @@ public class GTRNismo extends Car {
         public void visit(Node geom) {
             if (geom.getName().equals("ty")) {
                 w_fl_node.attachChild(geom);
-                geom.setLocalTranslation(0, 0, 0);
-                geom.center();
+                geom.setLocalTranslation(-2.2f, 0, 0);
             }
             else if (geom.getName().equals("ty.001")) {
                 w_fr_node.attachChild(geom);
-                geom.setLocalTranslation(0, 0, 0);
-                geom.center();
+                geom.setLocalTranslation(.2f, 0, 0);
             }
             else if (geom.getName().equals("ty.003")) {
                 w_rl_node.attachChild(geom);
                 geom.setLocalTranslation(0, 0, 0);
-                geom.center();
             }
             else if (geom.getName().equals("ty.002")) {
                 w_rr_node.attachChild(geom);
                 geom.setLocalTranslation(0, 0, 0);
-                geom.center();
             }
         }
     }
@@ -64,18 +60,18 @@ public class GTRNismo extends Car {
         w_rr_node.setLocalScale(scale);
 
 
-        addWheel(w_fr_node, new Vector3f(-0.94f, 0.3f, 1.4f), true, false, new Brake(140));
-        addWheel(w_fl_node, new Vector3f(0.94f, 0.3f, 1.4f), true, false, new Brake(140));
-        addWheel(w_rr_node, new Vector3f(-0.94f, 0.3f, -1.4f), false, false, new Brake(0));
-        addWheel(w_rl_node, new Vector3f(0.94f, 0.3f, -1.4f), false, false, new Brake(0));
+        addWheel(w_fr_node, new Vector3f(-0.84f, 0.1f, 1.4f), true, false, new Brake(140));
+        addWheel(w_fl_node, new Vector3f(0.84f, 0.1f, 1.4f), true, false, new Brake(140));
+        addWheel(w_rr_node, new Vector3f(-0.84f, 0.1f, -1.4f), false, false, new Brake(0));
+        addWheel(w_rl_node, new Vector3f(0.84f, 0.1f, -1.4f), false, false, new Brake(0));
 
         for (int i = 0; i < getNumWheels(); i++) {
-            getWheel(i).getSuspension().setRestLength(0.05f);
+            getWheel(i).getSuspension().setRestLength(0.01f);
 
-            getWheel(i).getSuspension().setStiffness(32);
-            getWheel(i).getSuspension().setMaxForce(12000);
-            getWheel(i).getSuspension().setCompression(0.5f);
-            getWheel(i).getSuspension().setDampness(0.65f);
+            getWheel(i).getSuspension().setStiffness(12.5f);
+            getWheel(i).getSuspension().setMaxForce(7000);
+            getWheel(i).getSuspension().setCompression(0.3f);
+            getWheel(i).getSuspension().setDampness(0.4f);
             getWheel(i).setFriction(1.6f);
         }
 
@@ -85,8 +81,8 @@ public class GTRNismo extends Car {
         getWheel(2).setTireModel(new Tyre_01());
         getWheel(3).setTireModel(new Tyre_01());
 
-        getWheel(0).setAccelerationForce(0);
-        getWheel(1).setAccelerationForce(0);
+        getWheel(0).setAccelerationForce(1);
+        getWheel(1).setAccelerationForce(1);
         getWheel(2).setAccelerationForce(1);
         getWheel(3).setAccelerationForce(1);
 
@@ -99,11 +95,11 @@ public class GTRNismo extends Car {
         setHoodCamLocation(new Vector3f(new Vector3f(0, 1.5f, 0.5f)));
 
         GearBox gearBox = new GearBox(5);
-        gearBox.setGear(0, 0, 15);
-        gearBox.setGear(1, 15, 40);
-        gearBox.setGear(2, 40, 75);
-        gearBox.setGear(3, 75, 130);
-        gearBox.setGear(4, 130, 190);
+        gearBox.setGear(0, 0, 30);
+        gearBox.setGear(1, 30, 70);
+        gearBox.setGear(2, 70, 130);
+        gearBox.setGear(3, 130, 190);
+        gearBox.setGear(4, 190, 255);
         // gearBox.setGear(5, 140, 220);
 
         setGearBox(gearBox);
