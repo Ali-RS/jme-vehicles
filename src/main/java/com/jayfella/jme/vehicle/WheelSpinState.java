18100ad67a364bf0e425ac7b21e0a5e7b2a0a9c9
jayfella <jayfella@gmail.com>
2019-11-09 21:30:46 +0000

    Toy around with a better equation.

--- src/main/java/com/jayfella/jme/vehicle/WheelSpinState.java
+++ src/main/java/com/jayfella/jme/vehicle/WheelSpinState.java
@@ -1,5 +1,6 @@
 package com.jayfella.jme.vehicle;
 
+import com.jayfella.jme.vehicle.part.Gear;
 import com.jayfella.jme.vehicle.part.Wheel;
 import com.jme3.app.Application;
 import com.jme3.app.state.BaseAppState;
@@ -48,10 +49,60 @@ public class WheelSpinState extends BaseAppState {
 
     }
 
+    // returns how many revolutions per second a wheel should be spinning at
+    // based on the gear and revs of the engine.
+    private float calcWheelspin(Wheel wheel) {
+
+        // https://sciencing.com/calculate-wheel-speed-7448165.html
+
+        Gear gear  = car.getGearBox().getActiveGear();
+
+        // should be between 0 and 1.
+        float revs = car.getEngine().getRevs();
+
+        // should give us the speed the vehicle should travel at these revs.
+        float mphRevs = FastMath.interpolateLinear(revs, gear.getStart(), gear.getEnd());
+
+        // convert mph to meters per minute
+        float metersPerHour = mphRevs * 1609;
+        float metersPerMin = metersPerHour / 60;
+
+        // calculate the circumference of the wheel.
+        float c = (wheel.getSize() * 0.5f) * FastMath.PI * 2;
+
+        // calc the wheel speed in revs per min
+        float revsPerMin = metersPerMin / c;
+        float revsPerSec = revsPerMin / 60;
+
+        return revsPerSec;
+    }
+
     @Override
     public void update(float tpf) {
 
         for (int i = 0; i < wheelCount; i++) {
+
+            /*
+            Wheel wheel = car.getWheel(i);
+
+            // rotation since last physics step
+            float currentRot = wheel.getVehicleWheel().getDeltaRotation();
+
+            // how many times this wheel "should" rotate per second.
+            float potentialRevsPerSec = calcWheelspin(wheel);
+
+            // multiply that by TWO_PI to get radians.
+            float potentialRot = potentialRevsPerSec / FastMath.TWO_PI;
+
+            // should give us a rotation in radians for "extre" rotation as a result
+            // of slip.
+            float diff = potentialRot - currentRot;
+
+            wheel.setRotationDelta(diff);
+
+            System.out.println(wheel.getVehicleWheel().getWheelSpatial().getName() + ": " + currentRot + " / " + potentialRot + " / " + diff);
+
+             */
 
             Wheel wheel = car.getWheel(i);
 
@@ -72,8 +123,15 @@ public class WheelSpinState extends BaseAppState {
             // set this before we do any "scene" modifications to make it look better.
             wheel.setRotationDelta(skidForce);
 
-            // so if we mult this by say 10 for 570 degrees and then mult it by tpf, we should be about right.
-            skidForce *= 20;
+            // the numbers below alter the scene only. they have no relation to any calculations.
+            // These calculations will add an additional rotation to the wheel to simulate wheelspin.
+
+            // so if we mult this by say 10(?) for 570 degrees and then mult it by tpf, we should be about right.
+            // this means if we are slipping 100% it will add ( 57 * x ) degrees per second.
+
+            // actually we can work this out. get the max revs.
+
+            skidForce *= 10;
             skidForce *= tpf;
 
             // the angle is negative due to the way the wheels rotate. negative is forward.
@@ -96,7 +154,8 @@ public class WheelSpinState extends BaseAppState {
             rot[i].fromAngles(angles[i]);
 
             wheelGeom.setLocalRotation(rot[i]);
-            // wheel.getVehicleWheel().addExtRotation(rot[i]);
+
+
         }
 
 
