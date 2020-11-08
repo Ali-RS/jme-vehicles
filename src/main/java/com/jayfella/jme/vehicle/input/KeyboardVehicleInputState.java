da975a37682484a959cf52c285bb3513e57b1bdc
Stephen Gold <sgold@sonic.net>
2020-11-07 22:33:34 -0800

    KeyboardVehicleInputState: formatting

--- src/main/java/com/jayfella/jme/vehicle/input/KeyboardVehicleInputState.java
+++ src/main/java/com/jayfella/jme/vehicle/input/KeyboardVehicleInputState.java
@@ -10,7 +10,9 @@ import com.jme3.math.Vector3f;
 import com.simsilica.lemur.GuiGlobals;
 import com.simsilica.lemur.input.*;
 
-public class KeyboardVehicleInputState extends BaseAppState implements StateFunctionListener {
+public class KeyboardVehicleInputState
+        extends BaseAppState
+        implements StateFunctionListener {
 
     public static final String G_VEHICLE = "GROUP_VEHICLE";
 
@@ -23,9 +25,7 @@ public class KeyboardVehicleInputState extends BaseAppState implements StateFunc
     private static final FunctionId F_HANDBRAKE = new FunctionId(G_VEHICLE, "Vehicle Handbrake");
 
     private static final FunctionId F_RESET = new FunctionId(G_VEHICLE, "Vehicle Reset");
-
     private static final FunctionId F_CAMVIEW = new FunctionId(G_VEHICLE, "Camera View");
-
     private static final FunctionId F_HORN = new FunctionId(G_VEHICLE, "Vehicle Horn");
 
     private final Vehicle vehicle;
@@ -54,76 +54,61 @@ public class KeyboardVehicleInputState extends BaseAppState implements StateFunc
 
     @Override
     protected void initialize(Application app) {
-
         inputMapper = GuiGlobals.getInstance().getInputMapper();
 
-        inputMapper.map( F_START_ENGINE, KeyInput.KEY_Y );
+        inputMapper.map(F_START_ENGINE, KeyInput.KEY_Y);
 
-        inputMapper.map( F_MOVE, KeyInput.KEY_W );
-        inputMapper.map( F_MOVE, InputState.Negative, KeyInput.KEY_S );
+        inputMapper.map(F_MOVE, KeyInput.KEY_W);
+        inputMapper.map(F_MOVE, InputState.Negative, KeyInput.KEY_S);
 
-        inputMapper.map( F_TURN, KeyInput.KEY_A );
-        inputMapper.map( F_TURN, InputState.Negative, KeyInput.KEY_D );
+        inputMapper.map(F_TURN, KeyInput.KEY_A);
+        inputMapper.map(F_TURN, InputState.Negative, KeyInput.KEY_D);
 
-        inputMapper.map( F_REVERSE, KeyInput.KEY_E );
-
-        inputMapper.map( F_HANDBRAKE, KeyInput.KEY_Q );
-
-        inputMapper.map( F_RESET, KeyInput.KEY_R );
-
-        inputMapper.map( F_CAMVIEW, KeyInput.KEY_F5);
-
-        inputMapper.map( F_HORN, KeyInput.KEY_H );
+        inputMapper.map(F_REVERSE, KeyInput.KEY_E);
+        inputMapper.map(F_HANDBRAKE, KeyInput.KEY_Q);
+        inputMapper.map(F_RESET, KeyInput.KEY_R);
+        inputMapper.map(F_CAMVIEW, KeyInput.KEY_F5);
+        inputMapper.map(F_HORN, KeyInput.KEY_H);
 
         inputMapper.addStateListener(this,
                 F_START_ENGINE, F_MOVE, F_TURN, F_REVERSE, F_HANDBRAKE, F_RESET,
-                F_HORN,
-                F_CAMVIEW
+                F_HORN, F_CAMVIEW
         );
 
         // activeCam = new VehicleFirstPersonCamera(vehicle, app.getCamera());
-
         setCamera(currentCam);
-
     }
 
     @Override
     protected void cleanup(Application app) {
+        inputMapper.removeMapping(F_START_ENGINE, KeyInput.KEY_Y);
 
-        inputMapper.removeMapping( F_START_ENGINE, KeyInput.KEY_Y );
+        inputMapper.removeMapping(F_MOVE, KeyInput.KEY_W);
+        inputMapper.removeMapping(F_MOVE, InputState.Negative, KeyInput.KEY_S);
 
-        inputMapper.removeMapping( F_MOVE, KeyInput.KEY_W );
-        inputMapper.removeMapping( F_MOVE, InputState.Negative, KeyInput.KEY_S );
+        inputMapper.removeMapping(F_TURN, KeyInput.KEY_A);
+        inputMapper.removeMapping(F_TURN, InputState.Negative, KeyInput.KEY_D);
 
-        inputMapper.removeMapping( F_TURN, KeyInput.KEY_A );
-        inputMapper.removeMapping( F_TURN, InputState.Negative, KeyInput.KEY_D );
-
-        inputMapper.removeMapping( F_REVERSE, KeyInput.KEY_E );
-
-        inputMapper.removeMapping( F_HANDBRAKE, KeyInput.KEY_SPACE );
-
-        inputMapper.removeMapping( F_RESET, KeyInput.KEY_R );
-
-        inputMapper.removeMapping( F_CAMVIEW, KeyInput.KEY_F5);
-
-        inputMapper.removeMapping( F_HORN, KeyInput.KEY_H );
+        inputMapper.removeMapping(F_REVERSE, KeyInput.KEY_E);
+        inputMapper.removeMapping(F_HANDBRAKE, KeyInput.KEY_SPACE);
+        inputMapper.removeMapping(F_RESET, KeyInput.KEY_R);
+        inputMapper.removeMapping(F_CAMVIEW, KeyInput.KEY_F5);
+        inputMapper.removeMapping(F_HORN, KeyInput.KEY_H);
 
         inputMapper.removeStateListener(this,
                 F_START_ENGINE, F_MOVE, F_TURN, F_REVERSE, F_HANDBRAKE, F_RESET,
-                F_HORN,
-                F_CAMVIEW
+                F_HORN, F_CAMVIEW
         );
-
     }
 
     @Override
     protected void onEnable() {
-        inputMapper.activateGroup( G_VEHICLE );
+        inputMapper.activateGroup(G_VEHICLE);
     }
 
     @Override
     protected void onDisable() {
-        inputMapper.deactivateGroup( G_VEHICLE );
+        inputMapper.deactivateGroup(G_VEHICLE);
     }
 
     @Override
@@ -134,9 +119,8 @@ public class KeyboardVehicleInputState extends BaseAppState implements StateFunc
         activeCam.update(tpf);
 
         //if (vehicle.getDriver() != null) {
-            //vehicle.getDriver().getPlayerNode().setLocalTranslation(vehicle.getLocation());
+        //vehicle.getDriver().getPlayerNode().setLocalTranslation(vehicle.getLocation());
         //}
-
     }
 
     boolean turningLeft, turningRight;
@@ -148,45 +132,36 @@ public class KeyboardVehicleInputState extends BaseAppState implements StateFunc
     boolean accelerating, braking, reversing;
 
     private void updateTurn(float tpf) {
-
         if (turningLeft) {
             steeringForce = Math.min(steeringForce + (tpf * turnSpeed), maxSteerForce);
             vehicle.steer(steeringForce);
-        }
-        else if (turningRight) {
+        } else if (turningRight) {
             steeringForce = Math.max(steeringForce - (tpf * turnSpeed), -maxSteerForce);
             vehicle.steer(steeringForce);
-        }
-        else {
+        } else {
             steeringForce = 0;
             vehicle.steer(steeringForce);
         }
     }
 
-
-
     public void updateMovement(float tpf) {
-
         // do braking first so it doesn't override engineBraking.
         if (braking) {
             vehicle.brake(1);
-        }
-        else {
+        } else {
             vehicle.brake(0);
         }
 
         if (accelerating) {
-
             vehicle.removeEngineBraking();
 
             if (vehicle.getSpeed(Vehicle.SpeedUnit.KMH) < vehicle.getGearBox().getMaxSpeed(Vehicle.SpeedUnit.KMH)) {
                 vehicle.accelerate(1);
-            }
-            else {
+            } else {
                 vehicle.accelerate(0);
             }
-        }
-        else {
+
+        } else {
             if (!braking) {
                 vehicle.applyEngineBraking();
             }
@@ -195,74 +170,53 @@ public class KeyboardVehicleInputState extends BaseAppState implements StateFunc
         }
 
         if (reversing) {
-
             if (vehicle.getSpeed(Vehicle.SpeedUnit.KMH) > -40) {
                 vehicle.accelerate(-1);
-            }
-            else {
+            } else {
                 vehicle.accelerate(0);
             }
-
         }
-
-
-
     }
 
     @Override
     public void valueChanged(FunctionId func, InputState value, double tpf) {
-
         boolean pressed = value == InputState.Positive;
 
         if (func == F_HORN) {
-
             if (pressed) {
                 // vehicle.setEngineStarted(!vehicle.getEngine().isStarted());
                 vehicle.pressHorn();
             }
-        }
 
-        else if (func == F_START_ENGINE) {
-
+        } else if (func == F_START_ENGINE) {
             if (!pressed) {
                 vehicle.setEngineStarted(!vehicle.getEngine().isStarted());
             }
-        }
 
-        else if (func == F_MOVE) {
-
+        } else if (func == F_MOVE) {
             if (value == InputState.Positive) {
                 accelerating = true;
-            }
-            else if (value == InputState.Negative) {
+            } else if (value == InputState.Negative) {
                 braking = true;
-            }
-            else {
+            } else {
                 accelerating = false;
                 braking = false;
 
                 // we are coasting. engine braking comes into force.
                 // braking applies to the wheels that are connected to the engine.
-
             }
-        }
 
-        else if (func == F_REVERSE) {
+        } else if (func == F_REVERSE) {
             reversing = value == InputState.Positive;
-        }
 
-        else if (func == F_HANDBRAKE) {
-
+        } else if (func == F_HANDBRAKE) {
             // if (pressed) {
-                // vehicle.setParkingBrakeApplied(!vehicle.isParkingBrakeApplied());
+            // vehicle.setParkingBrakeApplied(!vehicle.isParkingBrakeApplied());
             // }
             // vehicle.setParkingBrakeApplied(pressed);
             vehicle.handbrake(pressed ? 1 : 0);
 
-        }
-
-        else if (func == F_TURN) {
-
+        } else if (func == F_TURN) {
             if (value == InputState.Positive) {
                 turningLeft = true;
                 turningRight = false;
@@ -270,16 +224,13 @@ public class KeyboardVehicleInputState extends BaseAppState implements StateFunc
             } else if (value == InputState.Negative) {
                 turningRight = true;
                 turningLeft = false;
-            } else {
 
+            } else {
                 turningLeft = false;
                 turningRight = false;
-
             }
-        }
 
-        else if (func == F_RESET) {
-
+        } else if (func == F_RESET) {
             if (!pressed) {
                 return;
             }
@@ -294,23 +245,18 @@ public class KeyboardVehicleInputState extends BaseAppState implements StateFunc
             vehicle.getVehicleControl().setLinearVelocity(new Vector3f());
         }
 
-
         if (func == F_CAMVIEW && value == InputState.Positive) {
-
             currentCam = currentCam.next();
             setCamera(currentCam);
         }
-
     }
 
     private void setCamera(VehicleCamView camView) {
-
         if (activeCam != null) {
             activeCam.detach();
         }
 
         switch (camView) {
-
             case FirstPerson: {
                 activeCam = new VehicleFirstPersonCamera(vehicle, getApplication().getCamera());
                 break;
@@ -320,13 +266,11 @@ public class KeyboardVehicleInputState extends BaseAppState implements StateFunc
                 break;
             }
 
-            default: throw new IllegalArgumentException("Unknown Camera View: " + camView);
+            default:
+                throw new IllegalArgumentException(
+                        "Unknown Camera View: " + camView);
         }
 
         activeCam.attach();
-
     }
-
-
-
 }
