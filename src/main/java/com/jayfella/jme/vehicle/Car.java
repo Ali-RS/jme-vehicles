dcbba2e09360616420c5d13683b435dfd8fa2635
Stephen Gold <sgold@sonic.net>
2020-11-29 10:53:59 -0800

    Car: re-order fields and methods

--- src/main/java/com/jayfella/jme/vehicle/Car.java
+++ src/main/java/com/jayfella/jme/vehicle/Car.java
@@ -10,7 +10,6 @@ import com.jme3.bounding.BoundingBox;
 import com.jme3.bullet.objects.VehicleWheel;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Spatial;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,37 +17,28 @@ public class Car extends Vehicle {
 
     // wheel-related stuff. This isn't really "vehicle" related since a vehicle can be a boat or a helicopter.
     private final List<Wheel> wheels = new ArrayList<>();
+    private MagicFormulaState magicFormulaState;
+    private SkidMarksState skidmarks;
     /**
      * all available modes in the automatic transmission
      */
     final private String[] atModes = new String[]{"R", "D"};
     private TireSmokeEmitter smokeEmitter;
-    private SkidMarksState skidmarks;
     private WheelSpinState wheelSpinState;
-    private MagicFormulaState magicFormulaState;
 
     public Car(Application app, String name) {
         super(app, name);
     }
 
-    public int getNumWheels() {
-        return wheels.size();
-    }
-
-    public Wheel getWheel(int index) {
-        return wheels.get(index);
-    }
-
-    public Wheel addWheel(Spatial model, Vector3f connectionPoint, boolean isSteering, boolean steeringFlipped, Brake brake) {
-
+    public Wheel addWheel(Spatial model, Vector3f connectionPoint,
+            boolean isSteering, boolean steeringFlipped, Brake brake) {
         Vector3f direction = new Vector3f(0, -1, 0);
         Vector3f axle = new Vector3f(-1, 0, 0);
 
         float restlength = 0.2f;
-        float radius = ((BoundingBox)model.getWorldBound()).getZExtent();
+        float radius = ((BoundingBox) model.getWorldBound()).getZExtent();
 
         // boolean isFrontWheel = false;
-
         VehicleWheel vehicleWheel = getVehicleControl().addWheel(
                 model,
                 connectionPoint,
@@ -58,17 +48,24 @@ public class Car extends Vehicle {
                 radius,
                 isSteering);
 
-
         int index = getVehicleControl().getNumWheels() - 1;
 
         Suspension suspension = new Suspension(vehicleWheel, 0.2f, 0.3f);
 
-        Wheel wheel = new Wheel(getVehicleControl(), index, isSteering, steeringFlipped, suspension, brake);
-
+        Wheel wheel = new Wheel(getVehicleControl(), index, isSteering,
+                steeringFlipped, suspension, brake);
         wheels.add(wheel);
         getNode().attachChild(model);
 
         return wheel;
+    }
+
+    public int getNumWheels() {
+        return wheels.size();
+    }
+
+    public Wheel getWheel(int index) {
+        return wheels.get(index);
     }
 
     /**
@@ -83,6 +80,31 @@ public class Car extends Vehicle {
     public void removeWheel(int index) {
         getVehicleControl().removeWheel(index);
         this.wheels.remove(index);
+    }
+
+    public void setTireSkidMarksEnabled(boolean enabled) {
+        this.skidmarks.setSkidmarkEnabled(enabled);
+    }
+
+    public void setTireSkidMarksVisible(boolean enabled) {
+        this.skidmarks.setEnabled(enabled);
+    }
+
+    public void setTireSmokeEnabled(boolean enabled) {
+        this.smokeEmitter.setEnabled(enabled);
+    }
+
+    /**
+     * Determine the rotation angle for the steering wheel.
+     *
+     * @return the angle (in radians, negative = left, 0 = neutral, positive =
+     * right)
+     */
+    public float steeringWheelAngle() {
+        float steeringAngle = wheels.get(0).getSteeringAngle();
+        float result = 2f * steeringAngle;
+
+        return result;
     }
 
     @Override
@@ -102,8 +124,7 @@ public class Car extends Vehicle {
                     speedRatio = Math.max(0.25f, speedRatio);
 
                     wheel.accelerate(power * speedRatio);
-                }
-                else {
+                } else {
 
                     // we always set this because the wheel could be "broken down" over time.
                     wheel.accelerate(0);
@@ -113,63 +134,24 @@ public class Car extends Vehicle {
     }
 
     @Override
-    public void brake(float strength) {
+    public void applyEngineBraking() {
+        for (int i = 0; i < getNumWheels(); i++) {
+            Wheel wheel = getWheel(i);
 
+            // if the wheel is not "connected" to the engine, don't slow the wheel down using engine braking.
+            // so if the wheel has 1 acceleration force, apply full engine braking.
+            // but if the wheel has 0 acceleration force, it's not "connected" to the engine.
+            float brakingForce = getEngine().getBraking() * wheel.getAccelerationForce();
+            // System.out.println(brakingForce);
+            // wheel.brake(brakingForce);
+            getVehicleControl().brake(i, brakingForce);
+        }
+    }
+
+    @Override
+    public void brake(float strength) {
         for (Wheel wheel : wheels) {
             wheel.brake(strength);
-        }
-    }
-
-    @Override
-    public void handbrake(float strength) {
-        // just apply the brakes to the rear wheels.
-        wheels.get(2).brake(strength, 100);
-        wheels.get(3).brake(strength, 100);
-    }
-
-    @Override
-    public void steer(float strength) {
-        for (Wheel wheel : wheels) {
-            wheel.steer(strength);
-        }
-    }
-
-    /**
-     * Determine the rotation angle for the steering wheel.
-     *
-     * @return the angle (in radians, negative = left, 0 = neutral, positive =
-     * right)
-     */
-    public float steeringWheelAngle() {
-        float steeringAngle = wheels.get(0).getSteeringAngle();
-        float result = 2f * steeringAngle;
-
-        return result;
-    }
-
-    public void setTireSmokeEnabled(boolean enabled) {
-        this.smokeEmitter.setEnabled(enabled);
-    }
-
-    public void setTireSkidMarksVisible(boolean enabled) {
-        this.skidmarks.setEnabled(enabled);
-    }
-
-    public void setTireSkidMarksEnabled(boolean enabled) {
-        this.skidmarks.setSkidmarkEnabled(enabled);
-    }
-
-    @Override
-    public void setParkingBrakeApplied(boolean applied) {
-        super.setParkingBrakeApplied(applied);
-
-        if (applied) {
-            wheels.get(2).brake(1);
-            wheels.get(3).brake(1);
-        }
-        else {
-            wheels.get(2).brake(0);
-            wheels.get(3).brake(0);
         }
     }
 
@@ -188,17 +170,6 @@ public class Car extends Vehicle {
     }
 
     @Override
-    protected void enable() {
-        super.enable();
-
-        AppStateManager manager = Main.getApplication().getStateManager();
-        manager.attach(smokeEmitter);
-        manager.attach(skidmarks);
-        manager.attach(magicFormulaState);
-        manager.attach(wheelSpinState);
-    }
-
-    @Override
     protected void disable() {
         super.disable();
 
@@ -210,21 +181,21 @@ public class Car extends Vehicle {
     }
 
     @Override
-    public void applyEngineBraking() {
+    protected void enable() {
+        super.enable();
 
-        for (int i = 0; i < getNumWheels(); i++) {
-            Wheel wheel = getWheel(i);
+        AppStateManager manager = Main.getApplication().getStateManager();
+        manager.attach(smokeEmitter);
+        manager.attach(skidmarks);
+        manager.attach(magicFormulaState);
+        manager.attach(wheelSpinState);
+    }
 
-            // if the wheel is not "connected" to the engine, don't slow the wheel down using engine braking.
-            // so if the wheel has 1 acceleration force, apply full engine braking.
-            // but if the wheel has 0 acceleration force, it's not "connected" to the engine.
-
-            float brakingForce = getEngine().getBraking() * wheel.getAccelerationForce();
-            // System.out.println(brakingForce);
-            // wheel.brake(brakingForce);
-            getVehicleControl().brake(i, brakingForce);
-        }
-
+    @Override
+    public void handbrake(float strength) {
+        // just apply the brakes to the rear wheels.
+        wheels.get(2).brake(strength, 100);
+        wheels.get(3).brake(strength, 100);
     }
 
     @Override
@@ -234,5 +205,23 @@ public class Car extends Vehicle {
         }
     }
 
+    @Override
+    public void setParkingBrakeApplied(boolean applied) {
+        super.setParkingBrakeApplied(applied);
 
+        if (applied) {
+            wheels.get(2).brake(1);
+            wheels.get(3).brake(1);
+        } else {
+            wheels.get(2).brake(0);
+            wheels.get(3).brake(0);
+        }
+    }
+
+    @Override
+    public void steer(float strength) {
+        for (Wheel wheel : wheels) {
+            wheel.steer(strength);
+        }
+    }
 }
