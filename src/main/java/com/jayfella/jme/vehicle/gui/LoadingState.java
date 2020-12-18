24ccfae400201c60b5c7860f4dc94033fc3a8a1c
Stephen Gold <sgold@sonic.net>
2020-12-17 19:31:19 -0800

    LoadingState: re-order fields and methods

--- src/main/java/com/jayfella/jme/vehicle/gui/LoadingState.java
+++ src/main/java/com/jayfella/jme/vehicle/gui/LoadingState.java
@@ -29,10 +29,11 @@ public class LoadingState extends BaseAppState {
      */
     final private CountDownLatch latch;
 
-    private Node node = new Node("Loading Node");
+    private float speedMult = 3.0f;
     private Geometry backgroundGeom;
-    private Node spinnerNode = new Node("Spinner Node");
     private Label label;
+    private Node node = new Node("Loading Node");
+    private Node spinnerNode = new Node("Spinner Node");
     // *************************************************************************
     // constructors
 
@@ -43,6 +44,112 @@ public class LoadingState extends BaseAppState {
      */
     public LoadingState(CountDownLatch latch) {
         this.latch = latch;
+    }
+
+    public boolean isShowBackground() {
+        return backgroundGeom.getParent() != null;
+    }
+
+    public void setBackgroundColor(ColorRGBA color) {
+        backgroundGeom.getMaterial().setColor("Color", color);
+    }
+
+    public void setShowBackground(boolean value) {
+        if (value) {
+            node.attachChild(backgroundGeom);
+        } else {
+            backgroundGeom.removeFromParent();
+        }
+    }
+
+    public void setText(String text) {
+        label.setText(text);
+        label.setLocalTranslation(
+                getApplication().getCamera().getWidth() - label.getPreferredSize().x - 20,
+                label.getPreferredSize().y + 20,
+                1
+        );
+    }
+
+    /**
+     * Callback invoked after this AppState is detached or during application
+     * shutdown if the state is still attached. onDisable() is called before
+     * this cleanup() method if the state is enabled at the time of cleanup.
+     *
+     * @param app the application instance (not null)
+     */
+    @Override
+    protected void cleanup(Application app) {
+        // do nothing
+    }
+
+    /**
+     * Callback invoked during initialization once this AppState is attached but
+     * before onEnable() is called.
+     *
+     * @param app the application instance (not null)
+     */
+    @Override
+    protected void initialize(Application app) {
+        spinnerNode = createSpinnerNode(app.getAssetManager());
+        backgroundGeom = createBackgroundGeom(app.getAssetManager(), app.getCamera());
+        label = createLabel();
+
+        node.attachChild(backgroundGeom);
+        node.attachChild(spinnerNode);
+        node.attachChild(label);
+        node.setQueueBucket(RenderQueue.Bucket.Gui);
+
+        setText("Loading...");
+    }
+
+    /**
+     * Callback invoked whenever this AppState ceases to be both attached and
+     * enabled.
+     */
+    @Override
+    protected void onDisable() {
+        node.removeFromParent();
+    }
+
+    /**
+     * Callback invoked whenever this AppState becomes both attached and
+     * enabled.
+     */
+    @Override
+    protected void onEnable() {
+        ((SimpleApplication) getApplication()).getGuiNode().attachChild(node);
+    }
+
+    @Override
+    public void update(float tpf) {
+        spinnerNode.rotate(0, 0, -tpf * speedMult);
+
+        long latchCount = latch.getCount();
+        if (latchCount == 0L) {
+            /*
+             * All asynchronous asset loads have completed.
+             * Disable this AppState and display the main menu.
+             */
+            MainMenuState mainMenuState = new MainMenuState();
+            getStateManager().attach(mainMenuState);
+            setEnabled(false);
+        }
+    }
+
+    private Geometry createBackgroundGeom(AssetManager assetManager, Camera cam) {
+        Geometry result = new Geometry("Background", new Quad(cam.getWidth(), cam.getHeight()));
+        result.setMaterial(new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md"));
+        result.getMaterial().setColor("Color", new ColorRGBA(0.0f, 0.0f, 0.0f, 1.0f));
+        result.getMaterial().getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
+
+        return result;
+    }
+
+    private Label createLabel() {
+        label = new Label("");
+        label.setFontSize(18);
+        return label;
     }
 
     private Node createSpinnerNode(AssetManager assetManager) {
@@ -75,113 +182,5 @@ public class LoadingState extends BaseAppState {
         );
 
         return spinnerNode;
-    }
-
-    private Geometry createBackgroundGeom(AssetManager assetManager, Camera cam) {
-        Geometry result = new Geometry("Background", new Quad(cam.getWidth(), cam.getHeight()));
-        result.setMaterial(new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md"));
-        result.getMaterial().setColor("Color", new ColorRGBA(0.0f, 0.0f, 0.0f, 1.0f));
-        result.getMaterial().getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-
-        return result;
-    }
-
-    public void setShowBackground(boolean value) {
-        if (value) {
-            node.attachChild(backgroundGeom);
-        } else {
-            backgroundGeom.removeFromParent();
-        }
-    }
-
-    public boolean isShowBackground() {
-        return backgroundGeom.getParent() != null;
-    }
-
-    public void setBackgroundColor(ColorRGBA color) {
-        backgroundGeom.getMaterial().setColor("Color", color);
-    }
-
-    private Label createLabel() {
-        label = new Label("");
-        label.setFontSize(18);
-        return label;
-    }
-
-    public void setText(String text) {
-        label.setText(text);
-        label.setLocalTranslation(
-                getApplication().getCamera().getWidth() - label.getPreferredSize().x - 20,
-                label.getPreferredSize().y + 20,
-                1
-        );
-    }
-
-    /**
-     * Callback invoked during initialization once this AppState is attached but
-     * before onEnable() is called.
-     *
-     * @param app the application instance (not null)
-     */
-    @Override
-    protected void initialize(Application app) {
-        spinnerNode = createSpinnerNode(app.getAssetManager());
-        backgroundGeom = createBackgroundGeom(app.getAssetManager(), app.getCamera());
-        label = createLabel();
-
-        node.attachChild(backgroundGeom);
-        node.attachChild(spinnerNode);
-        node.attachChild(label);
-        node.setQueueBucket(RenderQueue.Bucket.Gui);
-
-        setText("Loading...");
-    }
-
-    /**
-     * Callback invoked after this AppState is detached or during application
-     * shutdown if the state is still attached. onDisable() is called before
-     * this cleanup() method if the state is enabled at the time of cleanup.
-     *
-     * @param app the application instance (not null)
-     */
-    @Override
-    protected void cleanup(Application app) {
-        // do nothing
-    }
-
-    /**
-     * Callback invoked whenever this AppState becomes both attached and
-     * enabled.
-     */
-    @Override
-    protected void onEnable() {
-        ((SimpleApplication) getApplication()).getGuiNode().attachChild(node);
-    }
-
-    /**
-     * Callback invoked whenever this AppState ceases to be both attached and
-     * enabled.
-     */
-    @Override
-    protected void onDisable() {
-        node.removeFromParent();
-    }
-
-    private float speedMult = 3.0f;
-
-    @Override
-    public void update(float tpf) {
-        spinnerNode.rotate(0, 0, -tpf * speedMult);
-
-        long latchCount = latch.getCount();
-        if (latchCount == 0L) {
-            /*
-             * All asynchronous asset loads have completed.
-             * Disable this AppState and display the main menu.
-             */
-            MainMenuState mainMenuState = new MainMenuState();
-            getStateManager().attach(mainMenuState);
-            setEnabled(false);
-        }
     }
 }
