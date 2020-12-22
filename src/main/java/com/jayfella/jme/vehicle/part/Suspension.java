fbb119fabe325cf21bff226674a41e6fdb00f2d4
Stephen Gold <sgold@sonic.net>
2020-12-22 13:01:02 -0800

    Suspension: comments

--- src/main/java/com/jayfella/jme/vehicle/part/Suspension.java
+++ src/main/java/com/jayfella/jme/vehicle/part/Suspension.java
@@ -3,16 +3,37 @@ package com.jayfella.jme.vehicle.part;
 import com.jme3.bullet.objects.VehicleWheel;
 import com.jme3.math.FastMath;
 
+/**
+ * Manage a wheel's suspension. If you use this class, you should avoid directly
+ * invoking the corresponding physics setters.
+ */
 public class Suspension {
+    // *************************************************************************
+    // fields
 
     private float stiffness;
     private float compression;
     private float damping;
     private float maxForce;
     private float restLength;
-
+    /**
+     * physics object being configured
+     */
     final private VehicleWheel vehicleWheel;
+    // *************************************************************************
+    // constructors
 
+    /**
+     * Instantiate a suspension with stiffness=25. TODO default to
+     * kCompress=0.2, kRelax=0.3
+     *
+     * @param vehicleWheel the corresponding physics object (not null, alias
+     * created)
+     * @param compression the desired damping ratio for compression (0=undamped,
+     * 1=critically damped)
+     * @param damping the desired damping ratio for relaxation (0=undamped,
+     * 1=critically damped)
+     */
     public Suspension(VehicleWheel vehicleWheel, float compression, float damping) {
         this.vehicleWheel = vehicleWheel;
 
@@ -29,11 +50,19 @@ public class Suspension {
         setDamping(this.damping);
         setMaxForce(10000);
     }
+    // *************************************************************************
+    // new methods exposed
 
     public float getStiffness() {
         return stiffness;
     }
 
+    /**
+     * Alter the stiffness of this suspension.
+     *
+     * @param stiffness the desired stiffness constant (10&rarr;off-road buggy,
+     * 50&rarr;sports car, 200&rarr;Formula-1 race car, default=5.88)
+     */
     public void setStiffness(float stiffness) {
         this.stiffness = stiffness;
         this.vehicleWheel.setSuspensionStiffness(stiffness);
@@ -46,6 +75,12 @@ public class Suspension {
         return compression;
     }
 
+    /**
+     * Alter the damping for compression
+     *
+     * @param compression the desired damping ratio (0=undamped, 1=critically
+     * damped)
+     */
     public void setCompression(float compression) {
         this.compression = compression;
         this.vehicleWheel.setWheelsDampingCompression(this.compression * 2.0f * FastMath.sqrt(stiffness));
@@ -55,6 +90,12 @@ public class Suspension {
         return damping;
     }
 
+    /**
+     * Alter the damping for relaxation. TODO rename
+     *
+     * @param damping the desired damping ratio (0=undamped, 1=critically
+     * damped)
+     */
     public void setDamping(float damping) {
         this.damping = damping;
         this.vehicleWheel.setWheelsDampingRelaxation(this.damping * 2.0f * FastMath.sqrt(stiffness));
