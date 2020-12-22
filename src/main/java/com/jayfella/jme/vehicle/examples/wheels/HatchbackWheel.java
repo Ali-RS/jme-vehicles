c92ec3091face0150a108dda80f4e0cf9cb68f2d
Stephen Gold <sgold@sonic.net>
2020-12-21 18:58:53 -0800

    +HatchbackWheel class to encapsulate "Modern Hatchback - Low Poly model"

---
+++ src/main/java/com/jayfella/jme/vehicle/examples/wheels/HatchbackWheel.java
@@ -0,0 +1,32 @@
+package com.jayfella.jme.vehicle.examples.wheels;
+
+import com.jayfella.jme.vehicle.Main;
+import com.jme3.asset.AssetManager;
+import com.jme3.scene.Spatial;
+import jme3utilities.Validate;
+
+/**
+ * Encapsulate a single wheel from Daniel Zhabotinsky's "Modern Hatchback - Low
+ * Poly" model.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class HatchbackWheel extends WheelModel {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a wheel with the specified diameter.
+     *
+     * @param diameter the desired diameter (in world units, &gt;0)
+     */
+    public HatchbackWheel(float diameter) {
+        Validate.positive(diameter, "diameter");
+
+        AssetManager assetManager = Main.getApplication().getAssetManager();
+        String assetPath = "/Models/modern_hatchback/wheel.j3o";
+        Spatial cgmRoot = assetManager.loadModel(assetPath);
+        super.setSpatial(cgmRoot);
+        super.getWheelNode().setLocalScale(diameter);
+    }
+}
