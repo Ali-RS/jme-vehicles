57c8c9943682316e58b00ff7091e14ae3bf36f2e
Stephen Gold <sgold@sonic.net>
2020-12-23 10:59:17 -0800

    WheelModel: re-order methods and add comments

--- src/main/java/com/jayfella/jme/vehicle/examples/wheels/WheelModel.java
+++ src/main/java/com/jayfella/jme/vehicle/examples/wheels/WheelModel.java
@@ -3,22 +3,53 @@ package com.jayfella.jme.vehicle.examples.wheels;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 
+/**
+ * A computer-graphics (C-G) model for a wheel.
+ *
+ * By convention, the +X axis points inward, toward the middle of the axle.
+ */
 abstract public class WheelModel {
+    // *************************************************************************
+    // fields
 
+    /**
+     * parent of the model's root spatial
+     */
     final private Node wheelNode = new Node("Wheel Node");
+    /**
+     * root spatial of the C-G model
+     */
     private Spatial wheelSpatial;
+    // *************************************************************************
+    // new methods exposed
 
-    protected void setSpatial(Spatial wheelSpatial) {
-        this.wheelSpatial = wheelSpatial;
-        this.wheelNode.attachChild(wheelSpatial);
-    }
-
+    /**
+     * Access the parent of the model's root spatial.
+     *
+     * @return the pre-existing Node (not null)
+     */
     public Node getWheelNode() {
         return wheelNode;
     }
 
+    /**
+     * Access the model's root spatial.
+     *
+     * @return the pre-existing instance
+     */
     public Spatial getSpatial() {
         return wheelSpatial;
     }
+    // *************************************************************************
+    // new protected methods
 
+    /**
+     * Initialize the C-G model.
+     *
+     * @param wheelSpatial the desired C-G model (not null)
+     */
+    protected void setSpatial(Spatial wheelSpatial) {
+        this.wheelSpatial = wheelSpatial;
+        this.wheelNode.attachChild(wheelSpatial);
+    }
 }
