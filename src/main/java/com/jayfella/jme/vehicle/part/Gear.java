9781e14e25c0160e591e4626efa52d4cd2691ac9
Stephen Gold <sgold@sonic.net>
2020-12-26 17:06:26 -0800

    Gear: rename fields and arguments, add comments and validation

--- src/main/java/com/jayfella/jme/vehicle/part/Gear.java
+++ src/main/java/com/jayfella/jme/vehicle/part/Gear.java
@@ -1,28 +1,76 @@
 package com.jayfella.jme.vehicle.part;
 
+import jme3utilities.Validate;
+
+/**
+ * A single configuration of a GearBox.
+ */
 public class Gear {
+    // *************************************************************************
+    // fields
 
-    private float start;
-    private float end;
+    /**
+     * vehicle speed that triggers a downshift (in KPH, &ge;0, &lt;maxKph)
+     */
+    private float minKph;
+    /**
+     * vehicle speed that triggers an upshift (in KPH, &gt;minKph) currently
+     * this corrsponds to engine redline
+     */
+    private float maxKph;
+    // *************************************************************************
+    // constructors
 
-    public Gear(float start, float end) {
-        this.start = start;
-        this.end = end;
+    /**
+     * Instantiate a Gear for the specified range of speeds.
+     *
+     * @param minKph speed that triggers a downshift (in KPH, &ge;0, &lt;maxKph)
+     * @param maxKph speed that triggers an upshift (in KPH, &gt;minKph)
+     */
+    public Gear(float minKph, float maxKph) {// TODO also specify name and ratio
+        Validate.inRange(minKph, "min kph", 0f, maxKph);
+
+        this.minKph = minKph;
+        this.maxKph = maxKph;
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Determine the vehicle speed that triggers a downshift.
+     *
+     * @return the speed (in KPH, &ge;0)
+     */
+    public float getStart() { // TODO rename getMinKph()
+        return minKph;
     }
 
-    public float getStart() {
-        return start;
+    /**
+     * Alter the vehicle speed that triggers a downshift.
+     *
+     * @param kph the desired speed (in KPH, &ge;0)
+     */
+    public void setStart(float kph) { // TODO rename setMinKph()
+        Validate.nonNegative(kph, "kph");
+        this.minKph = kph;
     }
 
-    public void setStart(float start) {
-        this.start = start;
+    /**
+     * Determine the vehicle speed that triggers an upshift.
+     *
+     * @return the speed (in KPH, &ge;0)
+     */
+    public float getEnd() { // TODO rename getMaxKph()
+        return maxKph;
     }
 
-    public float getEnd() {
-        return end;
-    }
-
-    public void setEnd(float end) {
-        this.end = end;
+    /**
+     * Alter the vehicle speed that triggers an upshift.
+     *
+     * @param kph the desired speed (in KPH, &ge;0)
+     */
+    public void setEnd(float kph) { // TODO rename setMaxKph()
+        Validate.nonNegative(kph, "kph");
+        this.maxKph = kph;
     }
 }
