ee06361ca82c0204b1d26baabdf5ea9fa093b22d
Stephen Gold <sgold@sonic.net>
2020-12-26 20:13:11 -0800

    engine examples: static points[], comments, and trivial changes

--- src/main/java/com/jayfella/jme/vehicle/examples/engines/Engine180HP.java
+++ src/main/java/com/jayfella/jme/vehicle/examples/engines/Engine180HP.java
@@ -5,39 +5,63 @@ import com.jayfella.jme.vehicle.engine.Engine;
 import com.jme3.math.Spline;
 import com.jme3.math.Vector3f;
 
+/**
+ * A 180-horsepower engine with a 600-RPM idle and a 5,000-RPM redline.
+ */
 public class Engine180HP extends Engine {
+    // *************************************************************************
+    // constants and loggers
 
     final private Vehicle vehicle;
-    final private Spline powerGraph;
+    /**
+     * points defining the power curve: x=RPMs, y=powerFraction
+     */
+    final private static Vector3f[] points = new Vector3f[]{
+        new Vector3f(0f, 0.25f, 0f),
+        new Vector3f(1_000, 0.35f, 0f),
+        new Vector3f(2_000, 0.4f, 0f),
+        new Vector3f(3_000, 0.6f, 0f),
+        new Vector3f(4_000, 0.7f, 0f),
+        new Vector3f(5_000, 0.95f, 0f),
+        new Vector3f(6_000, 0.99f, 0f),
+        new Vector3f(7_000, 0.95f, 0f),
+        new Vector3f(7_500, 0.85f, 0f)
+    };
+    // *************************************************************************
+    // fields
 
+    /**
+     * power curve
+     */
+    final private Spline spline;
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a 180-horsepower engine.
+     */
     public Engine180HP(Vehicle vehicle) {
         super("Basic 180", 1800f, 600f, 5000f, 10f);
         this.vehicle = vehicle;
-
-        Vector3f[] points = new Vector3f[] {
-
-                new Vector3f(0, 0.25f, 0),
-                new Vector3f(1000, 0.35f, 0),
-                new Vector3f(2000, 0.4f, 0),
-                new Vector3f(3000, 0.6f, 0),
-                new Vector3f(4000, 0.7f, 0),
-                new Vector3f(5000, 0.95f, 0),
-                new Vector3f(6000, 0.99f, 0),
-                new Vector3f(7000, 0.95f, 0),
-                new Vector3f(7500, 0.85f, 0),
-        };
-
-        powerGraph = new Spline(Spline.SplineType.Linear, points, 0.1f, false);
+        spline = new Spline(Spline.SplineType.Linear, points, 0.1f, false);
     }
+    // *************************************************************************
+    // Engine methods
 
     @Override
     public float getTorqueAtSpeed() {
         return getTorqueAtSpeed(vehicle);
     }
 
+    /**
+     * Determine the fractional power output at the specified speed. TODO rename
+     *
+     * @param rpm the angular speed of the crankshaft (in RPMs, &ge;0)
+     * @return the power as a fraction of the maximum (&ge;0, &le;1)
+     */
     @Override
-    public float evaluateSpline(float range) {
-        return evaluateSpline(powerGraph, range);
+    public float evaluateSpline(float rpm) {
+        float result = evaluateSpline(spline, rpm);
+        return result;
     }
-
 }
