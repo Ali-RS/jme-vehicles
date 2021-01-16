2cc61ff51fc73c63e5b64074816b0e5723cf5b8c
Stephen Gold <sgold@sonic.net>
2021-01-16 01:35:45 -0800

    MainMenu: re-label the stop button

--- src/main/java/com/jayfella/jme/vehicle/gui/menu/MainMenu.java
+++ src/main/java/com/jayfella/jme/vehicle/gui/menu/MainMenu.java
@@ -66,7 +66,7 @@ public class MainMenu extends AnimatedMenu {
         ));
         result.add(button);
 
-        button = new Button("Exit Game");
+        button = new Button("Quit the Demo");
         button.addClickCommands(source -> animateOut(()
                 -> application.stop()
         ));
