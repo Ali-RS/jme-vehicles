4ccc7cf8a8d2f1fa00d3fc8f69091c0ebd6d09c3
Stephen Gold <sgold@sonic.net>
2021-01-28 19:14:17 -0800

    create a Sound for "horn-1.ogg"

---
+++ MavCommon/src/main/java/com/jayfella/jme/vehicle/examples/sounds/HornSound1.java
@@ -0,0 +1,20 @@
+package com.jayfella.jme.vehicle.examples.sounds;
+
+import com.jayfella.jme.vehicle.Sound;
+
+/**
+ * A Sound built around "horn-1.ogg".
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class HornSound1 extends Sound {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate the Sound.
+     */
+    public HornSound1() {
+        super.addOgg("horn-1", 823f);
+    }
+}
