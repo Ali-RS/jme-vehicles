be11f5c67e8862a353244e6d615913ac70539516
Stephen Gold <sgold@sonic.net>
2021-07-08 15:18:37 -0700

    MavDemo2: add PropType enum

---
+++ MavDemo2/src/main/java/com/jayfella/jme/vehicle/niftydemo/state/PropType.java
@@ -0,0 +1,76 @@
+package com.jayfella.jme.vehicle.niftydemo.state;
+
+/**
+ * Enumerate prop types.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public enum PropType {
+    // *************************************************************************
+    // values
+
+    /**
+     * construction barrel with an orange base, from "Barrier & Traffic Cone
+     * Pack"
+     */
+    Barrel1,
+    /**
+     * construction barrel with a black base, from "Barrier & Traffic Cone Pack"
+     */
+    Barrel2,
+    /**
+     * Jersey barrier painted black, from "Barrier & Traffic Cone Pack"
+     */
+    Barrier,
+    /**
+     * Jersey barrier painted black and orange, from "Barrier & Traffic Cone
+     * Pack"
+     */
+    BarrierPainted,
+    /**
+     * traffic cone with a black base, from "Barrier & Traffic Cone Pack"
+     */
+    Cone1,
+    /**
+     * traffic cone with an orange base, from "Barrier & Traffic Cone Pack"
+     */
+    Cone2,
+    /**
+     * Jersey barrier topped with chain-link fence, from "Barrier & Traffic Cone
+     * Pack"
+     */
+    FencedBarrier,
+    /**
+     * tubular lane marker, from "Barrier & Traffic Cone Pack"
+     */
+    Marker,
+    /**
+     * low barrier painted black, from "Barrier & Traffic Cone Pack"
+     */
+    ShortBarrier,
+    /**
+     * low barrier painted black and orange, from "Barrier & Traffic Cone Pack"
+     */
+    ShortBarrierPainted,
+    /**
+     * low barrier painted black, orange, red, and white, from "Barrier &
+     * Traffic Cone Pack"
+     */
+    ShortBarrierSigned,
+    /**
+     * tall barrier painted black, from "Barrier & Traffic Cone Pack"
+     */
+    WallBarrier,
+    /**
+     * tall barrier painted black and orange, from "Barrier & Traffic Cone Pack"
+     */
+    WallBarrierPainted,
+    /**
+     * free-standing warning sign, from "Barrier & Traffic Cone Pack"
+     */
+    WarningSign,
+    /**
+     * iron weight, from "Barrier & Traffic Cone Pack"
+     */
+    Weight;
+}
