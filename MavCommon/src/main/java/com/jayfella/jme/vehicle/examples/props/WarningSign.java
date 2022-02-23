1f0ec6a0660e437857e3d106edb9369e65ee7c20
Stephen Gold <sgold@sonic.net>
2022-02-22 16:39:56 -0800

    MavCommon:  use Prop.isLoaded() for clarity

--- MavCommon/src/main/java/com/jayfella/jme/vehicle/examples/props/WarningSign.java
+++ MavCommon/src/main/java/com/jayfella/jme/vehicle/examples/props/WarningSign.java
@@ -59,7 +59,7 @@ public class WarningSign extends Prop {
      */
     @Override
     public void load(AssetManager assetManager) {
-        if (getMainBody() != null) {
+        if (isLoaded()) {
             logger2.log(Level.SEVERE, "Already loaded.");
             return;
         }
