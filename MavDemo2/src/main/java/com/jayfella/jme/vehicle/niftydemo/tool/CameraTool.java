b27192e8b83012f2905f8e2ede185b161d52adf3
Stephen Gold <sgold@sonic.net>
2022-11-22 17:11:41 -0800

    CameraTool:  replace the button with a bank of 4 radio buttons

--- MavDemo2/src/main/java/com/jayfella/jme/vehicle/niftydemo/tool/CameraTool.java
+++ MavDemo2/src/main/java/com/jayfella/jme/vehicle/niftydemo/tool/CameraTool.java
@@ -2,6 +2,7 @@ package com.jayfella.jme.vehicle.niftydemo.tool;
 
 import com.jayfella.jme.vehicle.niftydemo.view.CameraMode;
 import com.jayfella.jme.vehicle.niftydemo.view.Cameras;
+import java.util.List;
 import java.util.logging.Logger;
 import jme3utilities.nifty.GuiScreenController;
 import jme3utilities.nifty.Tool;
@@ -36,6 +37,51 @@ class CameraTool extends Tool {
     // Tool methods
 
     /**
+     * Enumerate this tool's radio buttons.
+     *
+     * @return a new list of names (unique id prefixes)
+     */
+    @Override
+    protected List<String> listRadioButtons() {
+        List<String> result = super.listRadioButtons();
+        result.add("cameraChase");
+        result.add("cameraDash");
+        result.add("cameraDynamic");
+        result.add("cameraOrbit");
+
+        return result;
+    }
+
+    /**
+     * Update the MVC model based on this tool's radio buttons.
+     *
+     * @param buttonName the name (unique id prefix) of the button (not null)
+     */
+    @Override
+    public void onRadioButtonChanged(String buttonName) {
+        switch (buttonName) {
+            case "cameraChase":
+                Cameras.setDesiredMode(CameraMode.Chase);
+                break;
+
+            case "cameraDash":
+                Cameras.setDesiredMode(CameraMode.Dash);
+                break;
+
+            case "cameraDynamic":
+                Cameras.setDesiredMode(CameraMode.Dynamic);
+                break;
+
+            case "cameraOrbit":
+                Cameras.setDesiredMode(CameraMode.Orbit);
+                break;
+
+            default:
+                super.onRadioButtonChanged(buttonName);
+        }
+    }
+
+    /**
      * Update this tool prior to rendering. (Invoked once per frame while this
      * tool is displayed.)
      */
@@ -43,6 +89,7 @@ class CameraTool extends Tool {
     protected void toolUpdate() {
         CameraMode mode = Cameras.getMode();
         String name = mode.toString();
-        setButtonText("cameraName", name);
+        String id = "camera" + name + "RadioButton";
+        getScreenController().setRadioButton(id);
     }
 }
